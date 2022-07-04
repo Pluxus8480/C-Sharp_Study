af8e1aac55d19e959652ab4b851d7fe1caf68a9c
Pluxus <kim84441@naver.com>
2022-07-05 00:56:24 +0900

    basic easy3
    boring

---
+++ Firststep/003_HelloWorld3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_HelloWorld3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" static void Main(String[] args)");
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("static void Main");
+        }
+
+        //static int Main()
+        //{
+        //    Console,WriteLIne("static int main");
+        //}
+
+    }
+}
