c8e004a5247c57c8095fd4958559fc5848f4b18a
Pluxus <kim84441@naver.com>
2022-11-23 08:10:09 +0900

    221123 c# data
    easy...

---
+++ Firststep/007_Variable2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _007_Variable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 100;
+            int y = 200;
+            int a = 1000, b = 2000;
+
+            Console.WriteLine("x: {0}", x);
+            Console.WriteLine("y: "+ y);
+            Console.WriteLine("a: "+ a);
+            Console.WriteLine("x: "+ b);
+        }
+    }
+}
